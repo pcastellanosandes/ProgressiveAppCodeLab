a75a559449369a77b1ddd7eff93f5a4c548f4b92
pcastellanosandes <pa.castellanos11@uniandes.edu.co>
2018-02-04 18:10:24 -0500

    register service worker

--- scripts/app.js
+++ scripts/app.js
@@ -342,4 +342,9 @@
   }
 
   // TODO add service worker code here
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+             .register('./serviceWorker.js')
+             .then(function() { console.log('Service Worker Registered'); });
+  }
 })();
